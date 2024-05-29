02a9ec740e57fe692fcb85b5ee95e9ad82181ae0
CodeWithSupriyo <supriyorana1st@gmail.com>
2024-05-29 12:32:48 +0530

    some funcnality added with JS

--- main.js
+++ main.js
@@ -29,3 +29,70 @@ closeNav.addEventListener("click", function() {
     close = 0
   }
 })
+
+const page2No1 = document.querySelector("#moreInfoinPage2No1")
+
+const page2No2 = document.querySelector("#moreInfoinPage2No2")
+
+const page2No3 = document.querySelector("#moreInfoinPage2No3")
+
+const page2No4 = document.querySelector("#moreInfoinPage2No4")
+
+const page2No5 = document.querySelector("#moreInfoinPage2No5")
+
+const btnPage2No1 = document.querySelector(".infoBtnPage2no1")
+
+const btnPage2No2 = document.querySelector(".infoBtnPage2no2")
+
+const btnPage2No3 = document.querySelector(".infoBtnPage2no3")
+
+const btnPage2No4 = document.querySelector(".infoBtnPage2no4")
+
+const btnPage2No5 = document.querySelector(".infoBtnPage2no5")
+
+btnPage2No2.addEventListener("click", function(){
+    page2No1.style.display = "none"
+    page2No3.style.display = "none"
+    page2No4.style.display = "none"
+    page2No5.style.display = "none"
+    btnPage2No1.style.background = "#F5F3F3"
+    btnPage2No1.style.border = "0.5px solid #000"
+    btnPage2No2.style.background = "#FBDDE0"
+    btnPage2No2.style.border = "none"
+})
+
+btnPage2No3.addEventListener("click", function() {
+  page2No1.style.display = "none"
+  page2No2.style.display = "none"
+  page2No3.style.display = "flex"
+  page2No4.style.display = "none"
+  page2No5.style.display = "none"
+  btnPage2No2.style.background = "#F5F3F3"
+  btnPage2No2.style.border = "0.5px solid #000"
+  btnPage2No3.style.background = "#FBDDE0"
+  btnPage2No3.style.border = "none"
+})
+
+btnPage2No4.addEventListener("click", function() {
+  page2No1.style.display = "none"
+  page2No2.style.display = "none"
+  page2No3.style.display = "none"
+  page2No4.style.display = "flex"
+  page2No5.style.display = "none"
+  btnPage2No3.style.background = "#F5F3F3"
+  btnPage2No3.style.border = "0.5px solid #000"
+  btnPage2No4.style.background = "#FBDDE0"
+  btnPage2No4.style.border = "none"
+})
+
+btnPage2No5.addEventListener("click", function() {
+  page2No1.style.display = "none"
+  page2No2.style.display = "none"
+  page2No3.style.display = "none"
+  page2No4.style.display = "none"
+  page2No5.style.display = "flex"
+  btnPage2No4.style.background = "#F5F3F3"
+  btnPage2No4.style.border = "0.5px solid #000"
+  btnPage2No5.style.background = "#FBDDE0"
+  btnPage2No5.style.border = "none"
+})
